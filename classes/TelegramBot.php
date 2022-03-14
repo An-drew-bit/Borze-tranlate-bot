90b66567dd3f46c88d417b1bfba9efbdaa959d0a
Andrew <mackarov.makoroff2016@yandex.ru>
2022-03-14 20:11:25 +0300

    add_project

---
+++ classes/TelegramBot.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace classes;
+
+use Telegram\Bot\Api;
+use Telegram\Bot\Objects\Message;
+
+class TelegramBot extends Api
+{
+    public function answerCallbackQuery(array $params)
+    {
+        $response = $this->post('answerCallbackQuery', $params);
+
+        return new Message($response->getDecodedBody());
+    }
+
+    public function btbSendRequest($method, array $params = [])
+    {
+        $response = $this->post($method, $params);
+
+        return new Message($response->getDecodedBody());
+    }
+}
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use classes\TelegramBot;
+use \Dejurin\GoogleTranslateForFree;
+
+$token = '?';
+
+$telegram = new TelegramBot($token);
+
+$update = $telegram->getWebhookUpdates();
+
+$chat_id = $update['message']['chat']['id'] ?? '';
+$text = $update['message']['text'] ?? '';
+
+if ($text == '/start') {
+
+    $response = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => 'Привет, готов переводить!',
+    ]);
+
+}  elseif (!empty($text)) {
+
+    if (preg_match('#[a-z]+#i', $text)) {
+        $source = 'en';
+        $target = 'ru';
+
+    } else {
+        $source = 'ru';
+        $target = 'en';
+    }
+
+    $attempts = 5;
+
+    $tr = new GoogleTranslateForFree();
+    $result = $tr->translate($source, $target, $text, $attempts);
+
+    if ($result) {
+        $response = $telegram->sendMessage([
+            'chat_id' => $chat_id,
+            'text' => $result,
+        ]);
+
+    } else {
+        $response = $telegram->sendMessage([
+            'chat_id' => $chat_id,
+            'text' => 'Упс.. Я не смог это перевести..',
+        ]);
+    }
+
+} else {
+    $response = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => 'Я текстовый бот-переводчик, пожалуйста напиши мне текст..',
+    ]);
+}
